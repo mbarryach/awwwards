7911f5bc6e02b2f94cb778753309b7db7a7c062d
Marc Barriach <mbr2809@gmail.com>
2026-05-07 23:58:26 +0200

    20260507
    title, start

--- assets/js/revealStart.js
+++ assets/js/revealStart.js
@@ -1,34 +1,62 @@
 gsap.registerPlugin(ScrollTrigger);
 
-let tlHero = gsap.timeline();
+const tl = gsap.timeline();
 
-tlHero
-.fromTo(".content h1", 
-    { y: -60, opacity: 0, filter: "blur(10px)"}, 
-    { y: 0, opacity: 1, duration: 1, ease: "power3.out",   filter: "blur(0px)" }
-)
+tl
+.fromTo(".intro-title", {
+    scale: 4,
+    opacity: 1,
+}, {
+    scale: 1,
+    duration: 2,
+    ease: "power4.inOut"
+})
+
+.to({}, { duration: 1 })
+
+.to(".intro-overlay", {
+    opacity: 0,
+    duration: 0.5
+}, "-=0.5")
+
+.fromTo(".content h1",
+{
+    opacity: 0
+},
+{
+    opacity: 1,
+    duration: 1.2,
+    ease: "power4.out"
+}, "-=0.2")
+
+.fromTo(".content h1",
+{
+    duration: 1,
+    ease: "power3.inOut"
+},
+{
+    y: -180,
+})
 
 .fromTo(".about p", 
-    { y: 40, opacity: 0, filter: "blur(10px)" }, 
+    { y: 50, opacity: 0, filter: "blur(10px)" }, 
     { 
-        y: 0, 
+        y: 400, 
         opacity: 1, 
         duration: 0.8, 
-        stagger: 0.2,
-        ease: "power3.out",
+        stagger: 0.15,
         filter: "blur(0px)"
     }, 
-    "-=0.5"
+    "-=0.4"
 )
 
 .fromTo(".my-name", 
-    { y: 40, opacity: 0, filter: "blur(10px)" }, 
+    { y: 30, opacity: 0, filter: "blur(10px)" }, 
     { 
         y: 0, 
         opacity: 1, 
         duration: 0.8, 
-        ease: "power3.out",
         filter: "blur(0px)" 
     }, 
-    "-=0.4"
+    "-=0.5"
 );
